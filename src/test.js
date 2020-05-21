46d014df5b345eda1e95f7b950150852ec371b3c
apartmentau <apartmentau@163.com>
2020-05-21 16:46:11 +0800

    update

--- src/test.js
+++ src/test.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App"
 
-ReactDOM.render(<h1>ddddd</h1>, document.getElementById("root"));
+ReactDOM.render(<App></App>, document.getElementById("root"));
